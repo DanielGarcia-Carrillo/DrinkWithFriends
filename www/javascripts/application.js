b7e4eab18698e1448ee9e0fc2eda37765f559a62
Daniel Garcia-Carrillo <garciacarrillo.daniel@gmail.com>
2013-12-03 00:06:20 -0600

    Added an MVC app to this steroids project which can be found in app/. Fixed moving from the index page to the app's homepage.

--- www/javascripts/application.js
+++ www/javascripts/application.js
@@ -1,3 +1,12 @@
+steroids.view.navigationBar.show("Drink with Friends");
 
-// steroids.view.navigationBar.show("Hello World");
-
+document.addEventListener("DOMContentLoaded", function() {
+  $("#opener").hammer().on("tap", function() {
+    var otherView = new steroids.views.WebView({
+      location: "/views/DrinkWithFriends/index.html"
+	});
+    steroids.layers.push({
+      view: otherView
+    });
+  });
+});
